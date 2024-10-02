e310c752c5f099d60eaa3126a20c6e8c0d645147
Pen9uin <sirpen9uin@gmail.com>
2024-10-02 23:26:25 +0300

    lint fix

--- src/js/api/ChatApi.js
+++ src/js/api/ChatApi.js
@@ -1,9 +1,9 @@
 export default class ChatApi {
-    constructor() { this._api = new WebSocket('wss://ahj-homeworks-sse-ws-chat-backend-64z9.onrender.com'); }
-  
-    addEventListener(type, callback, options = undefined) {
-      this._api.addEventListener(type, callback, options);
-    }
-  
-    send(data) { this._api.send(data); }
+  constructor() { this._api = new WebSocket('wss://ahj-homeworks-sse-ws-chat-backend-64z9.onrender.com'); }
+
+  addEventListener(type, callback, options = undefined) {
+    this._api.addEventListener(type, callback, options);
   }
+
+  send(data) { this._api.send(data); }
+}
